0e0e652bdb83b72818624712949e8a04796fa7a0
avgkudey <40860028+avgkudey@users.noreply.github.com>
2023-12-31 00:46:07 +0530

    render error page - WIP

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +29,40 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Prepare exception for rendering.
+     *
+     * @param $request
+     * @param Throwable $e
+     * @return JsonResponse|RedirectResponse|Response|\Symfony\Component\HttpFoundation\Response|\Inertia\Response
+     * @throws Throwable
+     */
+    public function render($request, Throwable $e): Response|JsonResponse|\Inertia\Response|\Symfony\Component\HttpFoundation\Response|RedirectResponse
+    {
+
+        if($e instanceof ErrorPageException){
+            return inertia('Error',
+                ['status' => $e->getCode(),
+                    'title'=>$e->error_data['title'],
+                    'description'=>$e->error_data['description']
+                ]);
+
+        }
+
+        $response = parent::render($request, $e);
+
+        if (! app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
+            return inertia('Error', ['status' => $response->status()])
+                ->toResponse($request)
+                ->setStatusCode($response->status());
+        } elseif ($response->status() === 419) {
+            return back()->with([
+                'message' => 'The page expired, please try again.',
+            ]);
+        }
+
+        return $response;
     }
 }
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({ status: Number },{ title: String|null },{ description: String|null })
+
+const title = computed(() => {
+    return {
+        503: '503: Service Unavailable',
+        500: props.title??'500: Server Error',
+        404: '404: Page Not Found',
+        403: '403: Forbidden',
+    }[props.status]
+})
+
+const description = computed(() => {
+    return {
+        503: 'Sorry, we are doing some maintenance. Please check back soon.',
+        500: props.description??'Whoops, something went wrong on our servers.',
+        404: 'Sorry, the page you are looking for could not be found.',
+        403: 'Sorry, you are forbidden from accessing this page.',
+    }[props.status]
+})
+</script>
+
+<template>
+    <div >
+        <H1>{{ title }}</H1>
+        <div>{{ description }}</div>
+    </div>
+</template>
