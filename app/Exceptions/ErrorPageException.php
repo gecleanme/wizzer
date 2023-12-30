7dadbc222e9e0fb14c32ae1e711c9464f4e53af3
avgkudey <40860028+avgkudey@users.noreply.github.com>
2023-12-31 00:45:46 +0530

    Create ErrorPageException.php

---
+++ app/Exceptions/ErrorPageException.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+class ErrorPageException extends Exception
+{
+    /**
+     * @var array{
+     *      title:string|null,
+     *      description:string|null,
+     *      icon:string|null,
+     *      type:string
+     *  }
+     */
+    public array $error_data=[];
+    /**
+     * @param array{
+     *     title:string|null,
+     *     description:string|null,
+     *     icon:string|null,
+     *     type:string
+     * } $data
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(array $data, int $code = 0, ?Throwable $previous = null)
+    {
+        $this->error_data=[
+            'title'=>$data['title']??null,
+            'description'=>$data['description']??null,
+            'icon'=>$data['icon']??null,
+            'type'=>$data['type']??null,
+        ];
+        parent::__construct($data['title'], $code, $previous);
+    }
+}
