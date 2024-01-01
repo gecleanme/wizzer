9683d602d5dc53ffa420d70075f6b2740bb06940
gecleanme <130738860+gecleanme@users.noreply.github.com>
2024-01-02 00:25:11 +0300

    ui tweak

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -47,9 +47,6 @@
                     >Powered by OpenWeatherMap API</a
                 >
             </p>
-            <p
-                class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
-            ></p>
         </div>
     </footer>
 </template>
